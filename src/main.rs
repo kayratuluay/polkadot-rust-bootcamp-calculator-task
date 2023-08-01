31c898497a9143c66b1806cc07f9aebe311bb8e5
Kayra <kayratuluay@gmail.com>
2023-08-01 12:07:46 +0300

    Main 3 | Operation enum and calculate function has been created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+enum Operation {
+    Add(f64, f64),
+    Subtract(f64, f64),
+    Multiply(f64, f64),
+    Divide(f64, f64),
+}
+
+fn calculate(operation: Operation) -> f64 {
+    match operation {
+        Operation::Add(first_number, second_number) => return first_number + second_number,
+        Operation::Subtract(first_number, second_number) => return first_number - second_number,
+        Operation::Multiply(first_number, second_number) => return first_number * second_number,
+        Operation::Divide(first_number, second_number) => return first_number / second_number,
+    }
+}
+
 fn main() {
     
 }
