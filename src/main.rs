f382b4bf9e7679b1e3a6b7d15ffafe453cd36485
kayratuluay <kayratuluay@gmail.com>
2023-08-04 12:09:06 +0300

    Main 5 | Console input has been implemented to the program.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 enum Operation {
     Add(f64, f64), // kacak
     Subtract(f64, f64),
@@ -14,13 +16,13 @@ fn calculate(operation: Operation) -> f64 {
     }
 }
 
-fn input_parser(mut string: String) -> Vec<String> {
-    string.push(' ');
+fn input_parser(string: String) -> Vec<String> {
+    let modified_string = string.as_str().to_owned() + " ";
     let mut result = Vec::new();
     let mut vessel = String::new();
     let mut space_checker = false;
 
-    for c in string.chars() {
+    for c in modified_string.chars() {
         if !c.is_whitespace() {
             space_checker = false;
             vessel.push(c);
@@ -38,21 +40,22 @@ fn input_parser(mut string: String) -> Vec<String> {
 fn main() {
     
     println!("Welcome to the CALCULATOR!");
-
-    loop {
-        let mut user_input = String::new();
-
-        
-        // let operations_display: [String; 4] = [""]
-
-        println!("Please enter a number");
-        user_input = get_input();
-        let first_num:f64 = user_input.parse().unwrap();
-
-        println!("{}", user_input);
-        break;
-        
+    let mut user_input = String::new();
+    match io::stdin().read_line(&mut user_input) {
+        Ok(n) => {
+            println!("{} bytes read.", n);
+            println!("{}", user_input);
+        },
+        Err(error) => println!("An error has been occured. {}", error),
     }
+    
+    
+
+    
+
+    let expression = input_parser(user_input);
+
+    println!("{:?}", expression);
 
 
 }
