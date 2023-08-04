c392b70044b880bc2427f65ba9be711e4ba8bce2
kayratuluay <kayratuluay@gmail.com>
2023-08-04 12:57:18 +0300

    Main 6 | Added operations on main function. The program is usable.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 enum Operation {
-    Add(f64, f64), // kacak
+    Add(f64, f64),
     Subtract(f64, f64),
     Multiply(f64, f64),
     Divide(f64, f64),
@@ -37,25 +37,43 @@ fn input_parser(string: String) -> Vec<String> {
     return result;
 }
 
+
 fn main() {
     
     println!("Welcome to the CALCULATOR!");
+    println!("Please enter an operation. example: 5 + 2");
     let mut user_input = String::new();
+
     match io::stdin().read_line(&mut user_input) {
         Ok(n) => {
             println!("{} bytes read.", n);
             println!("{}", user_input);
         },
-        Err(error) => println!("An error has been occured. {}", error),
+        Err(error) => println!("An error has been occured. {}", error)
     }
     
-    
-
-    
-
     let expression = input_parser(user_input);
 
-    println!("{:?}", expression);
+    let first_number: f64 = expression[0].parse().unwrap();
+    let operator: &str = &*expression[1];
+    let second_number: f64 = expression[2].parse().unwrap();
 
+    let mut resut_of_operation: f64 = 0.0;
+
+    match operator {
+        "+" => resut_of_operation = calculate(Operation::Add(first_number, second_number)),
+        "-" => resut_of_operation = calculate(Operation::Subtract(first_number, second_number)),
+        "*" | "x" => resut_of_operation = calculate(Operation::Multiply(first_number, second_number)),
+        "/" => {
+            if second_number > 0.0 {
+                resut_of_operation = calculate(Operation::Divide(first_number, second_number));
+            } else {
+                println!("{} can not be divided by 0!", first_number);
+            }
+        },
+        _ => println!("Please enter a expression by suggested method. example: 5 + 2")
+    }
+
+    println!("The result is {}", resut_of_operation);
 
 }
